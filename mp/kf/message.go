6035f38e878785ec55cac888fffb5c176f6f12d6
wu qingtao <wqt_110@qq.com>
2018-01-17 15:36:21 +0800

    新增客服消息接口

---
+++ mp/kf/message.go
@@ -0,0 +1,255 @@
+package kf
+
+const (
+	// 客服帐号管理接口path
+	wxKfPath    = "customservice/kfaccount"
+	wxKfAdd     = "add"
+	wxKfUpdate  = "update"
+	wxKfDel     = "del"
+	wxKfHeadImg = "uploadheadimg"
+	// 获取客服帐号和发送信息路径
+	wxKfGetAll = "cgi-bin/customservice/getkflist"
+	wxKfSend   = "cgi-bin/message/custom/send"
+	// 发送输入状态接口
+	wxKftyping = "cgi-bin/message/cutom/typing"
+)
+
+// Account 帐号管理
+type Account struct {
+	Kf_account string `json:"kf_account"`
+	Nickname   string `json:"nickname"`
+	Password   string `json:"password"`
+}
+
+// Response 微信返回错误码和信息
+type Response struct {
+	// Errcode 错误代码
+	Errcode int `json:"errcode"`
+	// Errmsg 错误消息
+	Errmsg string `json:"errmsg"`
+}
+
+// List getkflist返回的客户帐号信息
+type List struct {
+	Kf_account    string `json:"kf_account,omitempty"`
+	Kf_nick       string `json:"kf_nick,omitempty"`
+	Kf_id         string `json:"kf_id,omitempty"`
+	Kf_headimgurl string `json:"kf_headimgurl,omitempty"`
+}
+
+// Lists getkflist返回的客户帐号信息, 错误时返回错误码和错误信息
+type Lists struct {
+	Kf_list []*List `json:"kf_lsit,omitempty"`
+	Errcode int     `json:"errcode,omitempty"`
+	Errmsg  string  `json:"errmsg,omitempty"`
+}
+
+// Message 客服消息
+type Message struct {
+	// ToUser 消息接收方
+	ToUser string `json:"touser"`
+	// MsgType 消息类型
+	MsgType string `json:"msgtype"`
+	// Text 文本消息，MsgType为text
+	Text *Text `json:"text,omitempty"`
+	// Image 图片信息， MsgType为image
+	Image *Media `json:"image,omitempty"`
+	// Video 视频消息，MsgType为video
+	Video *Media `json:"video,omitempty"`
+	// Voice 语音消息，MsgType为voice
+	Voice *Media `json:"voice,omitempty"`
+	// Music 音乐消息，MsgType为music
+	Music *Music `json:"music,omitempty"`
+	// MpNews 图文消息，MsgType为mpnews
+	MpNews *Media `json:"mpnews,omitempty"`
+	// News 图文消息，点击跳转到外链
+	News *News `json:"news,omitempty"`
+	// WxCard 微信卡卷
+	WxCard *WxCard `json:"wxcard,omitempty"`
+	// MiniProgramPage 小程序页面
+	MiniProgramPage *MiniProgramPage `json:"miniprogrampage,omitempty"`
+	// CustomService 需要以某个客服帐号来发消息（在微信6.0.2及以上版本中显示自定义头像），则需在JSON数据包的后半部分加入customservice参数
+	CustomService *CustomService `json:"customservice,omitempty"`
+}
+
+// Text 文本消息
+type Text struct {
+	Content string `json:"content,omitempty"`
+}
+
+// Media 多媒体消息
+type Media struct {
+	MediaId      string `json:"media_id,omitempty"`
+	ThumbMediaId string `json:"thumb_media_id,omitempty"`
+	Title        string `json:"title,omitempty"`
+	Description  string `json:"description,omitempty"`
+}
+
+// NewMedia 新建多媒体消息
+func NewMedia(mediaid, title, desc, thumb string) *Media {
+	return &Media{mediaid, thumb, title, desc}
+}
+
+// Music 音乐消息
+type Music struct {
+	Title        string `json:"title,omitempty"`
+	Description  string `json:"description,omitempty"`
+	MusicUrl     string `json:"musicurl,omitempty"`
+	HqMusicUrl   string `json:"hqmusicurl"`
+	ThumbMediaId string `json:"thumb_media_id,omitempty"`
+}
+
+// NewMusic 新建音乐消息
+func NewMusic(title, desc, musicurl, hqmusicurl, thumb string) *Music {
+	return &Music{title, desc, musicurl, hqmusicurl, thumb}
+}
+
+// News图文消息
+type News struct {
+	Articles []*Article `json:"articles,omitempty"`
+}
+
+// Article 图文文章
+type Article struct {
+	Title       string `json:"title,omitempty"`
+	Description string `json:"description,omitempty"`
+	Url         string `json:"url,omitempty"`
+	PicUrl      string `json:"picurl,omitempty"`
+}
+
+// NewArticle 新建图文文章
+func NewArticle(title, desc, Url, picurl string) *Article {
+	return &Article{title, desc, Url, picurl}
+}
+
+// WxCard 微信卡卷
+type WxCard struct {
+	CardId string `json:"card_id,omitempty"`
+}
+
+// WxCard 新建客服消息微信卡卷消息
+func NewWxCard(id string) *WxCard {
+	return &WxCard{id}
+}
+
+// MiniProgramPage 微信小程序页面
+type MiniProgramPage struct {
+	Title        string `json:"title,omitempty"`
+	AppId        string `json:"appid,omitempty"`
+	PagePath     string `json:"pagepath,omitempty"`
+	ThumbMediaId string `json:"thumb_media_id,omitempty"`
+}
+
+// NewMiniProgramPage 新建小程序页面消息
+func NewMiniProgramPage(title, appid, pagepath, thumb string) *MiniProgramPage {
+	return &MiniProgramPage{title, appid, pagepath, thumb}
+}
+
+// CustomService 发送客服消息使用的客服帐号名称
+type CustomService struct {
+	Kf_account string `json:"kf_account,omitempty"`
+}
+
+// SetCustomService 设置使用哪个客服帐号发送消息
+func (msg *Message) SetCustomService(account string) {
+	if account != "" {
+		msg.CustomService = &CustomService{account}
+	}
+}
+
+// NewTextMessage 创建文本客服消息
+func NewTextMessage(touser, fromuser, content string) *Message {
+	msg := &Message{
+		ToUser:  touser,
+		MsgType: "text",
+		Text: &Text{
+			Content: content,
+		},
+	}
+	msg.SetCustomService(fromuser)
+	return msg
+}
+
+// newMediaMessage 新建多媒体消息
+func newMediaMessage(touser, fromuser, msgtype string, media *Media) *Message {
+	msg := &Message{
+		ToUser:  touser,
+		MsgType: msgtype,
+	}
+	switch msgtype {
+	case "image":
+		msg.Image = media
+	case "voice":
+		msg.Voice = media
+	case "video":
+		msg.Video = media
+	case "mpnews":
+		msg.MpNews = media
+	}
+	msg.SetCustomService(fromuser)
+	return msg
+}
+
+// NewImageMessage 创建图片客服消息
+func NewImageMessage(touser, fromuser string, media *Media) *Message {
+	return newMediaMessage(touser, fromuser, "image", media)
+}
+
+// NewVoiceMessage 创建语音客服消息
+func NewVoiceMessage(touser, fromuser string, media *Media) *Message {
+	return newMediaMessage(touser, fromuser, "voice", media)
+}
+
+// NewVideoMessage 创建视频客服消息
+func NewVideoMessage(touser, fromuser string, media *Media) *Message {
+	return newMediaMessage(touser, fromuser, "video", media)
+}
+
+// NewMusicMessage 创建音乐客服消息
+func NewMusicMessage(touser, fromuser string, music *Music) *Message {
+	msg := &Message{
+		ToUser:  touser,
+		MsgType: "music",
+		Music:   music,
+	}
+	msg.SetCustomService(fromuser)
+	return msg
+}
+
+// NewNewsMessage 创建图文（外链）客服消息
+func NewNewsMessage(touser, fromuser string, articles []*Article) *Message {
+	msg := &Message{
+		ToUser:  touser,
+		MsgType: "news",
+		News:    &News{articles},
+	}
+	msg.SetCustomService(fromuser)
+	return msg
+}
+
+// NewMpNewsMessage 创建图文客服消息
+func NewMpNewsMessage(touser, fromuser string, mpnews *Media) *Message {
+	return newMediaMessage(touser, fromuser, "mpnews", mpnews)
+}
+
+// NewWxCardMessage 创建微信卡卷客服消息
+func NewWxCardMessage(touser, fromuser string, wxcard *WxCard) *Message {
+	msg := &Message{
+		ToUser:  touser,
+		MsgType: "wxcard",
+		WxCard:  wxcard,
+	}
+	msg.SetCustomService(fromuser)
+	return msg
+}
+
+// NewMiniProgramPageMessage 创建微信小程序页面客服消息
+func NewMiniProgramPageMessage(touser, fromuser string, miniprogrampage *MiniProgramPage) *Message {
+	msg := &Message{
+		ToUser:          touser,
+		MsgType:         "miniprogrampage",
+		MiniProgramPage: miniprogrampage,
+	}
+	msg.SetCustomService(fromuser)
+	return msg
+}
---
+++ mp/kf/message_test.go
@@ -0,0 +1,82 @@
+package kf
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"testing"
+)
+
+func TestMsg(t *testing.T) {
+	var msgs = make([]*Message, 0)
+	t.Run("text", func(t *testing.T) {
+		msg := NewTextMessage("to_user", "from_user", "text_msg")
+		msgs = append(msgs, msg)
+	})
+	t.Run("image", func(t *testing.T) {
+		image := NewMedia("image_id", "", "", "")
+		msg := NewImageMessage("to_user", "from_user", image)
+		msgs = append(msgs, msg)
+	})
+	t.Run("voice", func(t *testing.T) {
+		voice := NewMedia("voice_id", "", "", "")
+		msg := NewVoiceMessage("to_user", "from_user", voice)
+		msgs = append(msgs, msg)
+	})
+	t.Run("video", func(t *testing.T) {
+		video := NewMedia("video_id", "video_title", "video_desc", "video_thumb")
+		msg := NewVideoMessage("to_user", "from_user", video)
+		msgs = append(msgs, msg)
+	})
+	t.Run("music", func(t *testing.T) {
+		music := NewMusic("music_title", "music_desc", "music_url", "music_hqurl", "music_thumb")
+		msg := NewMusicMessage("to_user", "from_user", music)
+		msgs = append(msgs, msg)
+	})
+	t.Run("news", func(t *testing.T) {
+		as := []*Article{
+			{
+				"a1_title",
+				"a1_desc",
+				"a1_picurl",
+				"a1_url",
+			},
+			{
+				"a2_title",
+				"a2_desc",
+				"a2_picurl",
+				"a2_url",
+			},
+		}
+		msg := NewNewsMessage("to_user", "from_user", as)
+		msgs = append(msgs, msg)
+	})
+
+	t.Run("mpnews", func(t *testing.T) {
+		mpnews := NewMedia("mpnews_id", "", "", "")
+		msg := NewMpNewsMessage("to_user", "from_user", mpnews)
+		msgs = append(msgs, msg)
+	})
+
+	t.Run("wxcard", func(t *testing.T) {
+		wxcard := NewWxCard("wxcard_id")
+		msg := NewWxCardMessage("to_user", "from_user", wxcard)
+		msgs = append(msgs, msg)
+	})
+
+	t.Run("miniprogrampage", func(t *testing.T) {
+		mini := NewMiniProgramPage("miniprogrampage_title", "appid",
+			"pagepath", "thumb")
+		msg := NewMiniProgramPageMessage("to_user", "from_user", mini)
+		msgs = append(msgs, msg)
+	})
+
+	for i := 0; i < len(msgs); i++ {
+		b, err := json.MarshalIndent(msgs[i], "", "  ")
+		if err != nil {
+			log.Fatalf("%3d xml MarshalIndent %s", i, err)
+		}
+		fmt.Printf("%3d:\n %s\n------------------------\n", i, b)
+
+	}
+}
